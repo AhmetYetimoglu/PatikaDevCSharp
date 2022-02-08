42233b3e0269ba27f6c5f9a33d5bb2e50bd3b112
= <=>
2022-02-09 00:24:29 +0300

    Koleksiyonlar 3. Soru Bitirildi

--- Koleksiyonlar-Soru-3/Program.cs
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -6,7 +6,37 @@ namespace Koleksiyonlar_Soru_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+
+            string[] kelimeler = Console.ReadLine().TrimEnd().Split(" ");
+            int sesliHarfSayisi = 0;
+            foreach (var item in kelimeler)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if(item[i]=='a' || item[i]=='e' || item[i]=='ı' || item[i]=='i' || item[i]=='o' || item[i]=='ö' || item[i]=='u' || item[i]=='ü')
+                        sesliHarfSayisi++;
+                }
+            }
+            char[] sesliHarfler = new char[sesliHarfSayisi];
+            string tekKelime ="";
+            int artanIndex = 0;
+            foreach (var item in kelimeler)
+            {
+                tekKelime+=item;
+            }
+            for (int i = 0; i < tekKelime.Length; i++)
+            {
+                if(tekKelime[i]=='a' || tekKelime[i]=='e' || tekKelime[i]=='ı' || tekKelime[i]=='i' || tekKelime[i]=='o' || tekKelime[i]=='ö' || tekKelime[i]=='u' || tekKelime[i]=='ü'){
+                    sesliHarfler[artanIndex] = tekKelime[i];
+                    artanIndex++;
+                }
+            }
+            foreach (var item in sesliHarfler)
+            {
+                Console.Write(item);
+            }
+            
         }
     }
 }
