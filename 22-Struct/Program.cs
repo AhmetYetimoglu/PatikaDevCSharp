5875b10870fc519f99f7190c542c18f92be0b1cb
= <=>
2022-06-26 01:08:02 +0300

    global.json, struıct and enum added

---
+++ 22-Struct/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _19_Struct
+{
+    class Program
+    {
+        // Struct ile Class arasındaki önemli farklardan biri Constructor'dır.
+        // Struct ile boş bir constructur oluşturulamaz. Class'ta oluşturulur.
+        // Eğer Class içindeki değişkenlere değer atanmadıysa o değişkenlere initial değer atanır ve işlem görür.
+        // Structa ise değişkenlere değer atanmadığı zaman initial değer almazlar.
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.KisaKenar = 3;
+            dikdortgen.UzunKenar = 4;
+            
+            Console.WriteLine("Class Alan Hesabı : {0}",dikdortgen.AlanHesapla());
+
+            Dikdortgen_Struct dikdortgen_struct;
+            dikdortgen_struct.KisaKenar = 3;
+            dikdortgen_struct.UzunKenar = 4;
+            
+            Console.WriteLine("Struct Alan Hesabı : {0}",dikdortgen_struct.AlanHesapla());
+        }
+    }
+
+    class Dikdortgen
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+
+    struct Dikdortgen_Struct
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        
+        }
+    }
+}
---
+++ 23-Enum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _20_Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sıcaklık = 32;
+
+            if(sıcaklık <= (int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim.");
+            else if(sıcaklık>=(int)HavaDurumu.Sıcak)
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+            else if(sıcaklık>=(int)HavaDurumu.Normal && sıcaklık<(int)HavaDurumu.CokSıcak)
+                Console.WriteLine("Hadi dışarıya çıkalım!");
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sıcak = 25,
+        CokSıcak = 30
+    }
+}
